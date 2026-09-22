f615e5b1524622c8522b966342be19f55d72232c
GoibaGoiba <enzzocossolino@gmail.com>
2026-09-22 13:49:33 -0300

    feat: Adicionar suporte ao sqflite e implementar funcionalidades de gerenciamento de dados

---
+++ lib/main_bcdTarefa.dart
@@ -0,0 +1,13 @@
+class Tarefa {
+  int? id;
+  String descricao;
+  String prioridade;
+  String status;
+
+  Tarefa({
+    this.id,
+    required this.descricao,
+    required this.prioridade,
+    required this.status,
+  });
+}
